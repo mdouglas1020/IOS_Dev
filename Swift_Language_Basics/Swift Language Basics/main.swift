7c5092c415b31e30543141f90dedf81479b42a79
Michael Douglas <michaeld1020@gmail.com>
2017-09-08 16:05:42 -0500

    Initial

---
+++ Swift_Language_Basics/Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Michael  Douglas on 9/8/17.
+//  Copyright © 2017 Michael Douglas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
